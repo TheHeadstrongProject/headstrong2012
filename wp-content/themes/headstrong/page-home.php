15cbe4d04dc15991a764eb5b7f4847432500c67b
Tom Xu <xudotcom@gmail.com>
2016-07-31 16:48:22 -0400

    Layout for different homepage styles above the fold

--- wp-content/themes/headstrong/page-home.php
+++ wp-content/themes/headstrong/page-home.php
@@ -7,8 +7,22 @@
 
 <?php get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-	<section class="standardHeroBanner" style="background-image: url(<?php echo wp_get_attachment_url(get_post_meta($post->ID, 'banner_image', true)); ?>)">
-		<?php echo get_post_meta($post->ID, 'banner_text', true); ?>
+	<section class="homepage__above-the-fold">
+	  <div class="homepage__panel">
+	    <div class="homepage__panel-content">
+	      <h1 class="homepage__panel-title">We've got your back.</h1>
+	      <p class="homepage__p">We provide free, confidential mental health support <i>that works,</i> for veterans of Iraq and Afghanistan.</p>
+	      <button class="homepage__button">Get Help</button>
+	    </div>
+	  </div>
+
+	  <div class="homepage__panel homepage__panel--secondary">
+	    <div class="homepage__panel-content homepage__panel-content--secondary">
+	      <h2 class="homepage__panel-title homepage__panel-title--secondary">Our Veterans need us.</h2>
+	      <p class="homepage__p">With your support, we can extend our proven program to reach more veterans in need.</p>
+	      <button class="homepage__button">Donate Now</button>
+	    </div>
+	  </div>
 	</section>
 
 	<div class="problems-bar">
