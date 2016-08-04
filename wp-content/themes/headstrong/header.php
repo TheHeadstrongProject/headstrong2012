4c87a9ee39b611276e79759cfd2fe7b1a432d335
Tom Xu <xudotcom@gmail.com>
2016-08-03 22:15:17 -0400

    add hony cta bar to bottom fo page

--- wp-content/themes/headstrong/header.php
+++ wp-content/themes/headstrong/header.php
@@ -13,21 +13,21 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="/favicon.png" />
 	<script>(function(){document.documentElement.className='js'})();</script>
 	<?php wp_head(); ?>
 	 <title><?php wp_title(); ?></title>
 	<!-- Bootstrap -->
-	<link href="<?php bloginfo('template_url'); ?>/css/styles.css" rel="stylesheet"> 
+	<link href="<?php bloginfo('template_url'); ?>/css/styles.css" rel="stylesheet">
 
 	<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-			
+
 	<![endif]-->
-		
-		
+
+
 	<!--[if gte IE 9]>
 	  <style type="text/css">
 	    .gradient {
@@ -37,19 +37,16 @@
 	<![endif]-->
 
 
-    
+
     <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,600italic,700,700italic,400italic,300,300italic' rel='stylesheet' type='text/css'>
-    
-    
-    
-    
+
+
+
+
 </head>
 
 <body <?php body_class(); ?>>
 
 	<?php get_template_part( 'navigation' ); ?>
-
-
-
-
+	<?php get_template_part('hony-cta'); ?>
---
+++ wp-content/themes/headstrong/hony-cta.php
@@ -0,0 +1,5 @@
+<?php if (!is_page('humans-of-new-york')) { ?>
+  <a class="hony-cta" href="/humans-of-new-york">
+    WE'RE ON <span class="hony-cta__text">HUMANS OF NEW YORK</span>
+  </a>
+<?php } ?>
