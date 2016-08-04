1f1ec39fa63150ac1c60197c4bd70d0edcd7ecd2
Eric Liu <dathtohn@gmail.com>
2016-08-04 19:58:21 -0400

    set up GoogleAnalytics js for tracking

--- wp-content/themes/headstrong/js-source/headstrong.js
+++ wp-content/themes/headstrong/js-source/headstrong.js
@@ -15,7 +15,7 @@ headstrong.equalBoxHeights = {
         })
     },
     evenHeights: function() {
-	    
+
         var t = this;
         clearTimeout(t.timer), t.timer = setTimeout(function() {
             t.resizeW()
@@ -82,110 +82,139 @@ headstrong.svgeezy = {
 }
 
 headstrong.validate = {
-	
+
 	init : function() {
-		
+
 		var _this = this;
-		
+
 		$(".validateField").each(function(){
-			
-			
+
+
 			$(this).on('blur',function(){
-				
+
 				_this.validateField($(this));
 			})
-			
+
 		});
-		
+
 	},
-	
+
 	validateField : function(field) {
-		
+
 		var type = field.attr('data-validationType');
 		var value = field.val();
 		var _this = this;
-		
+
 		var emailTest = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		
+
 		$('p.error',field.parent()).remove();
-		
+
 		switch (type) {
-			
+
 			case 'text' :
-			
+
 				if (value.trim() == '')
 				{
 					_this.addError(field);
-					
+
 					field.parent().addClass('has-error').removeClass('has-success');
 				} else {
-					
+
 					field.parent().removeClass('has-error').addClass('has-success');
 				}
-			
+
 				break;
-				
+
 			case 'email' :
-			
+
 				if (emailTest.test(value.trim()) == false)
 				{
 					_this.addError(field);
 					field.parent().addClass('has-error').removeClass('has-success');
 				} else {
-					
+
 					field.parent().removeClass('has-error').addClass('has-success');
 				}
-			
+
 				break;
 
 			case 'phone' :
-			
+
 				if (value.trim() == '')
 				{
 					_this.addError(field);
 					field.parent().addClass('has-error').removeClass('has-success');
 				} else {
-					
+
 					field.parent().removeClass('has-error').addClass('has-success');
-				} 
-				
+				}
+
 				break;
-			
+
 		}
-		
+
 	},
-	
+
 	addError : function(field) {
 
 		var _this = this;
 		field.after('<p class="error">' + field.attr('data-error') + '</span>');
 	}
-	
+
 }
 
+var GoogleAnalytics = (function() {
+  var sel = '.ga-track';
 
+  var init = function() {
+    console.info('The Headstrong Project: Google Analytics is ready.');
+    bindTracking();
+  };
 
+  var bindTracking = function() {
+    $(document).on('click', sel, function(e) {
+      var $t = $(this);
 
+      var action = $t.data('ga-category');
+      var category = $t.data('ga-category');
+      var label = $t.data('ga-category');
+
+      sendEvent(category, action, label);
+    });
+  };
+
+  var sendEvent = function(category, action, label) {
+    ga('send', 'event', category, action, label);
+  };
+
+  init();
+
+  return {
+    sendEvent: sendEvent
+  };
+}());
+
+// -------- document.ready
 
 $(document).ready(function(){
-	
+
 	headstrong.equalBoxHeights.init();
 	headstrong.svgeezy.init('nocheck','png');
 	 $('.navbar .dropdown > a').click(function(){
             location.href = this.href;
         });
-	
-	
+
+
 	$('.set-height').each(function(){
-		
+
 		$(this).css('min-height',$(this).height() + 'px');
-		
+
 	});
-	
+
 	$('.sm-button').on('click',function(e){
 		e.preventDefault();
 		e.stopPropagation();
         window.open(this.href, '_blank');
 	});
-		
+
 });
