c8cf6038d03eca05263fbcaeff20657f70596136
Tom Xu <xudotcom@gmail.com>
2016-08-05 21:45:16 -0400

    added background image and copy to hony page

--- wp-content/themes/headstrong/page-humans-of-new-york.php
+++ wp-content/themes/headstrong/page-humans-of-new-york.php
@@ -6,16 +6,48 @@
       <div class="row">
         <div class="col-xs-12">
           <div class="hony__content">
-              <h1 class="hony__title">Humans of New York</h1>
-              <p class="hony__p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-              <button class="hony__button">Fundraising Link?</button>
+            <h1 class="hony__title">We're on Humans of New York</h1>
+            <p class="hony__p">Out clients inspire us every day with stories worth sharing.</p>
+            <button class="hony__button">Get Help</button>
+            <button class="hony__button">Donate</button>
+          </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 
+
   <div class="container">
+
+    <div class="hony__body">
+      <div class="row">
+        <div class="col-xs-12">
+          <p class="hony__body-copy">
+            We’re honored that Humans of New York (HONY) is featuring our clients, the brave veterans of Iraq and Afghanistan, as well as the trauma clinicians who are moving mountains to help them.
+            <br />
+            <br />
+            Brandon Stanton started
+            <a href="http://www.humansofnewyork.com/">
+            HONY
+            </a>
+            as a simple photography project in 2010. Since then, his powerful storytelling has opened our hearts to strangers from every corner of the globe.
+            <br />
+            <br />
+            Now, he’s working with us to tell our
+            <a href="/about-us">
+            story
+            </a>
+            and change the lives of veterans in
+            <a href="/donate">
+            need.
+            </a>
+            The Headstrong Project provides free mental health care to veterans of Iraq and Afghanistan, delivered by world-class clinicians. And with your support, we can extend our proven program to reach more veterans in need.
+          </p>
+        </div>
+      </div>
+    </div>
+
     <div class="hony__profile">
       <div class="row">
         <div class="col-xs-12 col-md-4">
