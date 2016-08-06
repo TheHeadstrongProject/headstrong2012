54332737945eb55f6a47bc8c36c2926f3ac71a18
Eric Liu <ericliu@Eric-Lius-MBP.home>
2016-08-05 21:39:27 -0400

    style form

--- wp-content/themes/headstrong/get-help-form.php
+++ wp-content/themes/headstrong/get-help-form.php
@@ -11,40 +11,34 @@
     First Name
   </label>
   <input class="get-help-form__input" id="first_name" maxlength="40" name="first_name" size="20" type="text" />
-  <br />
 
   <label class="get-help-form__label" for="last_name">
     Last Name
   </label>
   <input class="get-help-form__input" id="last_name" maxlength="80" name="last_name" size="20" type="text" />
-  <br>
 
   <label class="get-help-form__label" for="email">
     Email
   </label>
   <input class="get-help-form__input" id="email" maxlength="80" name="email" size="20" type="email" />
-  <br>
 
   <label class="get-help-form__label" for="phone">
     Phone
   </label>
   <input class="get-help-form__input" id="phone" maxlength="40" name="phone" size="20" type="phone" />
-  <br>
 
   <label class="get-help-form__label" for="city">
     City
   </label>
-  <input id="city" maxlength="40" name="city" size="20" type="text" />
-  <br>
+  <input class="get-help-form__input" id="city" maxlength="40" name="city" size="20" type="text" />
 
   <label class="get-help-form__label" for="state">
     State/Province
   </label>
-  <input id="state" maxlength="20" name="state" size="20" type="text" />
-  <br>
+  <input class="get-help-form__input" id="state" maxlength="20" name="state" size="20" type="text" />
 
-  <label class="get-help-form__label" for="00N370000060fw7">Did you serve in Iraq or Afganistan?:</label>
-  <select id="00N370000060fw7" name="00N370000060fw7" title="Did you serve in Iraq or Afganistan?">
+  <label class="get-help-form__label" for="00N370000060fw7">Did you serve in Iraq or Afganistan?</label>
+  <select class="get-help-form__select" id="00N370000060fw7" name="00N370000060fw7" title="Did you serve in Iraq or Afganistan?">
     <option value="">
       --None--
     </option>
@@ -54,18 +48,17 @@
     <option value="No">
       No
     </option>
-    </select>
-    <br>
+  </select>
 
-  <label class="get-help-form__label" for="00N370000060fwC">Branch of Service:</label>
-  <input id="00N370000060fwC" maxlength="30" name="00N370000060fwC" size="20" type="text" />
-  <br>
+  <label class="get-help-form__label" for="00N370000060fwC">Branch of Service</label>
+  <input class="get-help-form__input" id="00N370000060fwC" maxlength="30" name="00N370000060fwC" size="20" type="text" />
 
-  <label class="get-help-form__label" for="00N370000060fwH">Message:</label>
-  <textarea id="00N370000060fwH" name="00N370000060fwH" rows="3" type="text" wrap="soft"></textarea>
-  <br>
+  <label class="get-help-form__label" for="00N370000060fwH">Message</label>
+  <textarea class="get-help-form__textarea" id="00N370000060fwH" name="00N370000060fwH" rows="3" type="text" wrap="soft"></textarea>
 
-  <input type="submit" name="submit">
+  <div class="get-help-form__submit-box">
+    <input class="get-help-form__submit" type="submit" name="submit">
+  </div>
 
   <!--
   <input type="hidden" name="debug" value=1>
--- wp-content/themes/headstrong/page-getintouch.php
+++ wp-content/themes/headstrong/page-getintouch.php
@@ -10,65 +10,42 @@
  * @subpackage Headstrong
  * @since Headstrong 1.0
  */
+?>
 
-get_header(); ?>
-
+<?php get_header(); ?>
 	<div class="getintouch-page">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
-
-
-
-
-?>	   <section class="overlapHeroBanner contactFormBanner">
-
-
-		   <div class="overlapHeroBannerImage" style="background-image: url(<?php echo wp_get_attachment_url(get_post_meta($post->ID, 'banner_image', true)); ?>)">
-
-		   </div>
-		   <div class="mobile-bg dark">
-			   &nbsp;
-		   </div>
-		   <div class="wrapper banner-body">
-			   <div class="container">
+		<?php while (have_posts()) : the_post(); ?>
+			<section class="overlapHeroBanner contactFormBanner">
+				<div class="overlapHeroBannerImage" style="background-image: url(<?php echo wp_get_attachment_url(get_post_meta($post->ID, 'banner_image', true)); ?>)">
+				</div>
+				<div class="mobile-bg dark">
+					&nbsp;
+				</div>
+				<div class="wrapper banner-body">
+					<div class="container">
 			   		<div class="row">
 				   		<div class="col-xs-12 col-md-10 col-md-offset-1 banner-text-header">
 		   			   <?php echo get_post_meta($post->ID, 'banner_text', true); ?>
 				   		</div>
 			   		</div>
-		   		</div>
-		   		<div class="get-help__form-box">
-						<?php get_template_part('get-help-form'); ?>
-	   			</div>
+
+			   		<div class="row">
+				   		<div class="col-xs-12 col-md-10 col-md-offset-1 banner-text-header">
+					   		<div class="get-help__form-box">
+									<?php get_template_part('get-help-form'); ?>
+				   			</div>
+			   			</div>
+		   			</div>
+					</div>
 				</div>
+			</section>
 
-   </section>
+			<div class="wrapper padded-panel extra-padded-panel">
+				<?php echo get_post_meta($post->ID, 'get_in_touch_form', true); ?>
+			</div>
 
+			<?php echo get_post_meta($post->ID, 'page_scripts', true); ?>
 
-<div class="wrapper padded-panel extra-padded-panel">
-<?php echo get_post_meta($post->ID, 'get_in_touch_form', true); ?>
-
-
-</div>
-
- <?php echo get_post_meta($post->ID, 'page_scripts', true); ?>
-
-
-
-<?php
-
-
-
-		// End the loop.
-		endwhile;
-		?>
-
-
-
-
-
-
+		<?php endwhile; ?>
 	</div>
-
 <?php get_footer(); ?>
