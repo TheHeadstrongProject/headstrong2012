0cd5a6180ea4d1b6fbd50423bd47f6c87ecd426e
Eric Liu <ericliu@Eric-Lius-MBP.home>
2016-08-07 10:32:19 -0400

    make fields into half width

--- wp-content/themes/headstrong/get-help-form.php
+++ wp-content/themes/headstrong/get-help-form.php
@@ -8,51 +8,67 @@
   <input type="hidden" id="lead_source" name="lead_source" value="Web to Lead" />
   <input type="hidden" name="retURL" value="<?php echo home_url() . '/get-help'; ?>">
 
-  <label class="get-help-form__label" for="first_name">
-    First Name
-  </label>
-  <input class="get-help-form__input" id="first_name" maxlength="40" name="first_name" size="20" type="text" />
+  <div class="get-help-form__group">
+    <label class="get-help-form__label" for="first_name">
+      First Name
+    </label>
+    <input class="get-help-form__input" id="first_name" maxlength="40" name="first_name" size="20" type="text" />
+  </div>
 
-  <label class="get-help-form__label" for="last_name">
-    Last Name
-  </label>
-  <input class="get-help-form__input" id="last_name" maxlength="80" name="last_name" size="20" type="text" />
+  <div class="get-help-form__group">
+    <label class="get-help-form__label" for="last_name">
+      Last Name
+    </label>
+    <input class="get-help-form__input" id="last_name" maxlength="80" name="last_name" size="20" type="text" />
+  </div>
 
-  <label class="get-help-form__label" for="email">
-    Email
-  </label>
-  <input class="get-help-form__input" id="email" maxlength="80" name="email" size="20" type="email" />
+  <div class="get-help-form__group">
+    <label class="get-help-form__label" for="email">
+      Email
+    </label>
+    <input class="get-help-form__input" id="email" maxlength="80" name="email" size="20" type="email" />
+  </div>
 
-  <label class="get-help-form__label" for="phone">
-    Phone
-  </label>
-  <input class="get-help-form__input" id="phone" maxlength="40" name="phone" size="20" type="phone" />
+  <div class="get-help-form__group">
+    <label class="get-help-form__label" for="phone">
+      Phone
+    </label>
+    <input class="get-help-form__input" id="phone" maxlength="40" name="phone" size="20" type="phone" />
+  </div>
 
-  <label class="get-help-form__label" for="city">
-    City
-  </label>
-  <input class="get-help-form__input" id="city" maxlength="40" name="city" size="20" type="text" />
+  <div class="get-help-form__group">
+    <label class="get-help-form__label" for="city">
+      City
+    </label>
+    <input class="get-help-form__input" id="city" maxlength="40" name="city" size="20" type="text" />
+  </div>
 
-  <label class="get-help-form__label" for="state">
-    State/Province
-  </label>
-  <input class="get-help-form__input" id="state" maxlength="20" name="state" size="20" type="text" />
+  <div class="get-help-form__group">
+    <label class="get-help-form__label" for="state">
+      State/Province
+    </label>
+    <input class="get-help-form__input" id="state" maxlength="20" name="state" size="20" type="text" />
+  </div>
 
-  <label class="get-help-form__label" for="00N370000060fw7">Did you serve in Iraq or Afganistan?</label>
-  <select class="get-help-form__select" id="00N370000060fw7" name="00N370000060fw7" title="Did you serve in Iraq or Afganistan?">
-    <option value="">
-      --None--
-    </option>
-    <option value="Yes">
-      Yes
-    </option>
-    <option value="No">
-      No
-    </option>
-  </select>
+  <div class="get-help-form__group">
+    <label class="get-help-form__label" for="00N370000060fw7">Did you serve in Iraq or Afganistan?</label>
+    <select class="get-help-form__select" id="00N370000060fw7" name="00N370000060fw7" title="Did you serve in Iraq or Afganistan?">
+      <option value="">
+        --None--
+      </option>
+      <option value="Yes">
+        Yes
+      </option>
+      <option value="No">
+        No
+      </option>
+    </select>
+  </div>
 
-  <label class="get-help-form__label" for="00N370000060fwC">Branch of Service</label>
-  <input class="get-help-form__input" id="00N370000060fwC" maxlength="30" name="00N370000060fwC" size="20" type="text" />
+  <div class="get-help-form__group">
+    <label class="get-help-form__label" for="00N370000060fwC">Branch of Service</label>
+    <input class="get-help-form__input" id="00N370000060fwC" maxlength="30" name="00N370000060fwC" size="20" type="text" />
+  </div>
 
   <label class="get-help-form__label" for="00N370000060fwH">Message</label>
   <textarea class="get-help-form__textarea" id="00N370000060fwH" name="00N370000060fwH" rows="3" type="text" wrap="soft"></textarea>
