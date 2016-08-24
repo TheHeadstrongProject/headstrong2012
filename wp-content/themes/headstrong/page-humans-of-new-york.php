287fc19d10be6bb3f60be914df4a81077bd1e814
Eric Liu <ericliu@Eric-Lius-MacBook-Pro.local>
2016-08-24 18:30:28 -0400

    add Derek

--- wp-content/themes/headstrong/page-humans-of-new-york.php
+++ wp-content/themes/headstrong/page-humans-of-new-york.php
@@ -339,6 +339,21 @@
 
       <div class="row">
         <div class="col-xs-12">
+          <div class="hony__feature">
+            <h2 class="hony__h2">Derek, U.S. Marine Corps</h2>
+            <div class="row">
+              <div class="col-xs-12 col-md-4">
+                <div class="hony__fb-embed hony__fb-embed--feature">
+                  <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fhumansofnewyork%2Fposts%2F1345378262202937%3A0&width=500" width="100%" height="537" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12">
           <div class=hony__body-video>
             <p class="hony__body-video-title">
               Learn more about Headstrong:
