23163e0515112c1d9d1bfa0a3af34ffa1a4dd424
Tom Xu <xudotcom@gmail.com>
2016-12-12 17:42:11 -0500

    minor fixes to bottom cta

--- wp-content/themes/headstrong/bottom-cta.php
+++ wp-content/themes/headstrong/bottom-cta.php
@@ -6,16 +6,18 @@
     data-ga-label="fixed"
     href="/donate"
   >
-    <div class="bottom-cta__text">Get us to 50K by 2017</div>
+    <div class="bottom-cta__text">Get us to 50k by 2017 help us and donate now</div>
     <div class="bottom-cta__button">Donate</div>
+
     <div class="progress bottom-cta__thermometer">
       <div class="active progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%">
-      <span class="sr-only">70% Complete</span>
+      <span>$25,000</span>
       </div>
     </div>
-    <div class="bottom-cta__thermometer">
-    <div class="bottom-cta__thermometer-min">$0</div>
-    <div class="bottom-cta__thermometer-max">$50,000</div>
+
+    <div class="bottom-cta__thermometer-values">
+      <div class="bottom-cta__thermometer-min">$0</div>
+      <div class="bottom-cta__thermometer-max">$50,000</div>
     </div>
   </a>
 <?php } ?>
