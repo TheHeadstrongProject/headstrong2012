a74e91ca787878c5c118bf831e8b7a3027e70577
Tom Xu <xudotcom@gmail.com>
2016-12-12 23:17:36 -0500

    removed hover and link from bottom-cta on donation page

--- wp-content/themes/headstrong/bottom-cta.php
+++ wp-content/themes/headstrong/bottom-cta.php
@@ -1,12 +1,12 @@
 <?php if (!is_page('donate')) { ?>
   <a
-    class="ga-track bottom-cta"
+    class="ga-track bottom-cta bottom-cta--link"
     data-ga-action="clicked on link"
     data-ga-category="donate"
     data-ga-label="fixed"
     href="/donate"
   >
-    <div class="bottom-cta__text">Get us to 50k by 2017 help us and donate now</div>
+    <div class="bottom-cta__text">Get us to 50k by 2017</div>
     <div class="bottom-cta__button">Donate</div>
 
     <div class="progress bottom-cta__thermometer">
@@ -20,4 +20,21 @@
       <div class="bottom-cta__thermometer-max">$50,000</div>
     </div>
   </a>
+<?php } ?>
+
+<?php if (is_page('donate')) { ?>
+  <div class="bottom-cta">
+    <div class="bottom-cta__text bottom-cta__text--donation">Get us to 50k by 2017</div>
+
+    <div class="progress bottom-cta__thermometer">
+      <div class="active progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+      <span>$25,000</span>
+      </div>
+    </div>
+
+    <div class="bottom-cta__thermometer-values">
+      <div class="bottom-cta__thermometer-min">$0</div>
+      <div class="bottom-cta__thermometer-max">$50,000</div>
+    </div>
+  </div>
 <?php } ?>
